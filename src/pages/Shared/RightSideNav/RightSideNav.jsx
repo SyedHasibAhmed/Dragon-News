a3581a045715763653d681f832a96006e45313de
SyedHasibAhmed <syedhasibahmed11@gmail.com>
2024-03-20 03:37:20 +0600

    right navbar q zone

--- src/pages/Shared/RightSideNav/RightSideNav.jsx
+++ src/pages/Shared/RightSideNav/RightSideNav.jsx
@@ -6,6 +6,10 @@ import {
   FaInstagram,
 } from "react-icons/fa6";
 
+import qZone1 from "../../../assets/qZone1.png";
+import qZone2 from "../../../assets/qZone2.png";
+import qZone3 from "../../../assets/qZone3.png";
+
 const RightSideNav = () => {
   return (
     <div>
@@ -40,8 +44,18 @@ const RightSideNav = () => {
           href=""
         >
           <FaInstagram className="mr-2"></FaInstagram>
-          <span>Facebook</span>
+          <span>Instagram</span>
         </a>
+      </div>
+
+      {/* q zone */}
+      <div className="p-4 space-y-3 mb-6">
+        <div>
+          <h2 className="text-3xl">Q Zone</h2>
+          <img src={qZone1} alt="" />
+          <img src={qZone2} alt="" />
+          <img src={qZone3} alt="" />
+        </div>
       </div>
     </div>
   );
