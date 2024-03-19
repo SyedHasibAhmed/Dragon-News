392bee550d655ba0ce15ed7bd2e187101b286cce
SyedHasibAhmed <syedhasibahmed11@gmail.com>
2024-03-20 03:25:26 +0600

    RightNavBar all button set with react icons

--- src/pages/Shared/RightSideNav/RightSideNav.jsx
+++ src/pages/Shared/RightSideNav/RightSideNav.jsx
@@ -1,7 +1,42 @@
+import {
+  FaGoogle,
+  FaGithub,
+  FaFacebook,
+  FaTwitter,
+  FaInstagram,
+} from "react-icons/fa6";
+
 const RightSideNav = () => {
   return (
     <div>
-      <h2 className="text-3xl">Right Side</h2>
+      {/* Login With */}
+      <div className="p-4 space-y-3 mb-6">
+        <h2 className="text-3xl text-center">Login with</h2>
+        <button className="btn btn-outline w-full">
+          <FaGoogle></FaGoogle>
+          Google
+        </button>
+        <button className="btn btn-outline w-full">
+          <FaGithub></FaGithub>
+          GitHub
+        </button>
+      </div>
+      {/* Find Us On */}
+      <div className="p-4 space-y-3 mb-6">
+        <h2 className="text-3xl text-bold text-center">Find Us On</h2>
+        <button className="btn btn-outline w-full">
+          <FaFacebook></FaFacebook>
+          FaceBook
+        </button>
+        <button className="btn btn-outline w-full">
+          <FaTwitter></FaTwitter>
+          Twitter
+        </button>
+        <button className="btn btn-outline w-full">
+          <FaInstagram></FaInstagram>
+          Instagram
+        </button>
+      </div>
     </div>
   );
 };
