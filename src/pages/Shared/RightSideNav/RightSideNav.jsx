9f80e90ac29701530344d6bfc7ef5f548f3609d8
SyedHasibAhmed <syedhasibahmed11@gmail.com>
2024-03-20 03:30:26 +0600

    Right NavBar Update

--- src/pages/Shared/RightSideNav/RightSideNav.jsx
+++ src/pages/Shared/RightSideNav/RightSideNav.jsx
@@ -22,20 +22,26 @@ const RightSideNav = () => {
         </button>
       </div>
       {/* Find Us On */}
-      <div className="p-4 space-y-3 mb-6">
-        <h2 className="text-3xl text-bold text-center">Find Us On</h2>
-        <button className="btn btn-outline w-full">
-          <FaFacebook></FaFacebook>
-          FaceBook
-        </button>
-        <button className="btn btn-outline w-full">
-          <FaTwitter></FaTwitter>
-          Twitter
-        </button>
-        <button className="btn btn-outline w-full">
-          <FaInstagram></FaInstagram>
-          Instagram
-        </button>
+      <div className="p-4 mb-6">
+        <h2 className="text-3xl mb-4">Find Us on</h2>
+        <a
+          className="p-4 flex text-lg items-center border rounded-t-lg"
+          href=""
+        >
+          <FaFacebook className="mr-3"></FaFacebook>
+          <span>Facebook</span>
+        </a>
+        <a className="p-4 flex text-lg items-center border-x" href="">
+          <FaTwitter className="mr-2"></FaTwitter>
+          <span>Twitter</span>
+        </a>
+        <a
+          className="p-4 flex text-lg items-center border rounded-b-lg"
+          href=""
+        >
+          <FaInstagram className="mr-2"></FaInstagram>
+          <span>Facebook</span>
+        </a>
       </div>
     </div>
   );
